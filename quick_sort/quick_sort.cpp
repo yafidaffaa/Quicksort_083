8d6b7b86178087fd99277e2ec5b9c6a74ba85e2b
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-03-31 14:58:50 +0700

    Add project files.

---
+++ quick_sort/quick_sort.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+//array of integers to hold value
+int arr[20];
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movment
+int n;
