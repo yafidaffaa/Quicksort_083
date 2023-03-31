4d1a2f1f22ea7480c780402c2c4cb5d86d4b5ced
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-03-31 15:02:52 +0700

    membuat sebuah function yang tidak mengembalikan nilai

--- quick_sort/quick_sort.cpp
+++ quick_sort/quick_sort.cpp
@@ -7,3 +7,16 @@ int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movment
 int n;
 
+void main() {
+	while (true) 
+	{
+		cout << "Masukan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMasukan panjang array adalah 0" << endl;
+	}
+
+}
